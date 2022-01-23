98bf573f8ef8363f4e77b2d6d1ae4c051182ed3f
eminebasarici <eminebasarici@gmail.com>
2022-01-23 19:24:51 +0300

    Commit

---
+++ FirebaseChat/app/src/main/java/com/firebase/chat/model/Status.java
@@ -0,0 +1,13 @@
+package com.firebase.chat.model;
+
+
+
+public class Status{
+    public boolean isOnline;
+    public long timestamp;
+
+    public Status(){
+        isOnline = false;
+        timestamp = 0;
+    }
+}
